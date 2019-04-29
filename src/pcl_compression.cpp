4c04a81cc7729d3f3880ad31a23d73dbe05a19a0
Marcelino <marcelino.malmeidan@gmail.com>
2019-04-29 16:24:58 -0500

    added max-min distance filtering

--- src/pcl_compression.cpp
+++ src/pcl_compression.cpp
@@ -15,6 +15,9 @@ class PclCompression
   ros::Publisher pcl_pub_;
   std::string input_topic_, out_topic_;
   double resolution_;
+  int drop_factor_;
+  int cur_idx_;
+  float min_distance_, max_distance_;
 
 public:
   PclCompression(ros::NodeHandle *nh) {
@@ -23,6 +26,10 @@ public:
     nh_.getParam("input_topic", input_topic_);
     nh_.getParam("out_topic", out_topic_);
     nh_.getParam("resolution", resolution_);
+    nh_.getParam("max_distance", max_distance_);
+    nh_.getParam("min_distance", min_distance_);
+    nh_.getParam("drop_factor", drop_factor_);
+    cur_idx_ = 0;
 
     pcl_sub_ = nh_.subscribe(input_topic_, 1, &PclCompression::pcl_callback, this);
     pcl_pub_ = nh_.advertise<sensor_msgs::PointCloud2>(out_topic_, 1);
@@ -30,6 +37,9 @@ public:
     ROS_INFO("[pcl_compression] Input PCL topic: %s", pcl_sub_.getTopic().c_str());
     ROS_INFO("[pcl_compression] Output PCL topic: %s", pcl_pub_.getTopic().c_str());
     ROS_INFO("[pcl_compression] Compression voxel size: %f", resolution_);
+    ROS_INFO("[pcl_compression] Minimum point distance: %f", min_distance_);
+    ROS_INFO("[pcl_compression] Maximum point distance: %f", max_distance_);
+    ROS_INFO("[pcl_compression] Drop factor: %d", drop_factor_);
   }
 
   ~PclCompression() {
@@ -37,10 +47,18 @@ public:
   }
 
   void pcl_callback(const sensor_msgs::PointCloud2::ConstPtr &msg) {
+    // Only execute the function when cur_idx == 0
+    if (cur_idx_ != 0) {
+      cur_idx_++;
+      return;
+    } else if (cur_idx_ == drop_factor_-1) {
+      cur_idx_ = 0;
+    }
+
     // Container for original & filtered data
     pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
     pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-    pcl::PCLPointCloud2 cloud_filtered;
+    pcl::PCLPointCloud2 cloud_filtered, cloud_out;
 
     // Convert to PCL data type
     pcl_conversions::toPCL(*msg, *cloud);
@@ -49,6 +67,7 @@ public:
     pcl::VoxelGrid<pcl::PCLPointCloud2> grid;
     grid.setInputCloud(cloudPtr);
     grid.setLeafSize(resolution_, resolution_, resolution_);
+    grid.setFilterLimits(min_distance_, max_distance_);
     grid.filter(cloud_filtered);
 
     // Convert to ROS data type
